86e62e933ac2f373ac7a9805cc4e82a9d7e26c2d
Johnson Apanishile <djsaint03@yahool.com>
2021-03-21 00:06:44 +0200

    added a template design to login and registration and more features

--- auction/views.py
+++ auction/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,Http404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+from .forms import CreateUserForm
+
+
 
 def loginpage(request):
     return render(request, 'accounts/login.html')
 
 def registerpage(request):
-    return render(request, 'accounts/register.html')
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get("username") #to get username
+            messages.success(request, "Account created for ", + user)
+            return redirect("login")
+
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
 
 def home(request):
     return render(request, "home.html")
